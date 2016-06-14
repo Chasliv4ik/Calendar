f8215db4ef9810fb8a312cb95786f737f0ed4bbe
Chasliv4ik <Savvadson@gmail.com>
2016-06-14 19:11:41 +0300

    u

--- src/Calendar.java
+++ src/Calendar.java
@@ -4,24 +4,27 @@ import java.time.Month;
 import java.time.Year;
 
 public class Calendar {
+
     public void getCalendar() {
 
         LocalDate date = LocalDate.now();
-        DayOfWeek firstDayOfWeek = DayOfWeek.from(LocalDate.of(date.getYear(), date.getMonth(), 1));
+        int firstDayOfWeek = DayOfWeek.from(LocalDate.of(date.getYear(), date.getMonth(), 1)).getValue();
+
         showHeadCalendar(date.getMonth(), Year.of(date.getYear()),firstDayOfWeek);
+
         for (int i = 1; i <= date.lengthOfMonth(); i++) {
             if (i == date.getDayOfMonth()) {
                 System.out.print((char) 27 + "[31m");
                 System.out.printf("%3d ", i);
                 System.out.print((char) 27 + "[37m");
             } else {
-                if (((i + firstDayOfWeek.getValue()) % 7 == 0) || ((i - 1 + firstDayOfWeek.getValue()) % 7 == 0) && (i != firstDayOfWeek.getValue())) {
-                    System.out.print((char) 27 + "[34m");
+                if (((i + firstDayOfWeek) % 7 == 0) || ((i - 1 + firstDayOfWeek) % 7 == 0) && (i != firstDayOfWeek)) {
+                   textBlue();
                 }
                 System.out.printf("%3d ", i);
             }
-            if (((i - 1 + firstDayOfWeek.getValue()) % 7 == 0) || (i == date.lengthOfMonth()))
-                System.out.println((char) 27 + "[37m");
+         textWhite(i,firstDayOfWeek,date.lengthOfMonth());
+
         }
 
     }
@@ -29,29 +32,38 @@ public class Calendar {
     public void getCalendar(int month) {
 
         Month m = Month.of(month);
-
         Year y = Year.of(LocalDate.now().getYear());
+        int lengthMonth = m.length(LocalDate.now().isLeapYear());
+        int  firstDayOfWeek = DayOfWeek.from(LocalDate.of(y.getValue(), m.getValue(), 1)).getValue();
 
-        DayOfWeek firstDayOfWeek = DayOfWeek.from(LocalDate.of(y.getValue(), m.getValue(), 1));
         showHeadCalendar(m,y,firstDayOfWeek);
-        for (int i = 1; i <= m.length(LocalDate.now().isLeapYear()); i++) {
 
-            if ((i + firstDayOfWeek.getValue()) % 7 == 0) System.out.print((char) 27 + "[34m");
+        for (int i = 1; i <=lengthMonth; i++) {
 
+            if ((i + firstDayOfWeek) % 7 == 0) textBlue();
             System.out.printf("%3d ", i);
 
-            if (((i - 1 + firstDayOfWeek.getValue()) % 7 == 0) || (i == m.length(LocalDate.now().isLeapYear())))
-                System.out.println((char) 27 + "[37m");
+               textWhite(i,firstDayOfWeek,lengthMonth);
         }
 
     }
 
-    public void showHeadCalendar(Month m,Year y,DayOfWeek d){
+    public void showHeadCalendar(Month m,Year y,int d){
         System.out.println(" CALENDAR FOR " + m + " " + y + " ");
 
         System.out.println(" Mon Tue Wed Thu Fri" + (char) 27 + "[34m Sat Sun " + (char) 27 + "[37m");
 
-        for (int i = 1; i < d.getValue(); i++)
+        for (int i = 1; i < d; i++)
             System.out.print("    ");
+    }
+
+    public void textBlue(){
+        System.out.print((char) 27 + "[34m");
+    }
+    public void textWhite(int i,int d,int lengthMonth){
+        if (((i - 1 + d) % 7 == 0) || (i == lengthMonth)) {
+            System.out.println();
+            System.out.print((char) 27 + "[37m");
+        }
     }
 }
